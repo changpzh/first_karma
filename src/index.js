196ae9e9c23e79a2e6762894290d3099c5d18f35
Changping <changping.zhou@foxmail.com>
2017-07-04 18:30:33 +0800

    first commit for first_karma

---
+++ src/index.js
@@ -0,0 +1,4 @@
+function reverse(name) {
+	console.log("reverse test!");
+	return name.split('').reverse().join('');
+}
---
+++ test/unit/TestCase1.js
@@ -0,0 +1,14 @@
+describe("A suite", function() {
+	it("contains spec with an expectation", function() {
+		console.log("this is msg from log...");
+		expect(true).toBe(true);
+	}); 
+});
+
+describe("A suite of basic functions", function(){
+	it("reverse word", function() {
+		expect("DCBA").toEqual(reverse("ABCD"));
+		expect("demo").toEqual(reverse("omed11"));
+		console.log('test');
+	});
+});
